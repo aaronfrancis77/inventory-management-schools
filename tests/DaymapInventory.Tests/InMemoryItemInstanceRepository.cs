72b3a39dd3e544aede6dba59a3f9825f9750c944
sarahma45 <105347702@student.swin.edu.au>
2026-05-05 20:37:38 +1000

    Test for ItemInstances

--- tests/DaymapInventory.Tests/InMemoryItemInstanceRepository.cs
+++ tests/DaymapInventory.Tests/InMemoryItemInstanceRepository.cs
@@ -1,42 +1,40 @@
 using DaymapInventory.Interfaces;
 using DaymapInventory.Models;
 
-namespace DaymapInventory.Repositories
+namespace DaymapInventory.Repositories;
+
+public class InMemoryItemInstanceRepository : IItemInstanceRepository
 {
-    public class InMemoryItemInstanceRepository : IItemInstanceRepository
+    private readonly List<ItemInstance> _itemInstances = new();
+    private int _nextId = 1;
+
+    public void Add(ItemInstance instance)
     {
-        private readonly List<ItemInstance> _itemInstances = new();
-        private int _nextId = 1;
+        instance.Id = _nextId++;
+        _itemInstances.Add(instance);
+    }
 
-        public void Add(ItemInstance instance)
+    public IEnumerable<ItemInstance> GetAll()
+    {
+        return _itemInstances.AsReadOnly();
+    }
+
+    public ItemInstance? GetById(int id)
+    {
+        return _itemInstances.FirstOrDefault(i => i.Id == id);
+    }
+
+    public IEnumerable<ItemInstance> GetByItemId(int itemId)
+    {
+        return _itemInstances.Where(i => i.ItemId == itemId);
+    }
+
+    public void Delete(int id)
+    {
+        var instance = GetById(id);
+        if (instance != null)
         {
-            instance.Id = _nextId++;
-            _itemInstances.Add(instance);
-        }
-
-        public IEnumerable<ItemInstance> GetAll()
-        {
-            return _itemInstances;
-        }
-
-        public ItemInstance? GetById(int id)
-        {
-            return _itemInstances.FirstOrDefault(i => i.Id == id);
-        }
-
-        public IEnumerable<ItemInstance> GetByItemId(int itemId)
-        {
-            return _itemInstances.Where(i => i.ItemId == itemId);
-        }
-
-        public void Delete(int id)
-        {
-            var instance = GetById(id);
-
-            if (instance != null)
-            {
-                _itemInstances.Remove(instance);
-            }
+            _itemInstances.Remove(instance);
         }
     }
 }
--- tests/DaymapInventory.Tests/InMemoryItemInstanceRepositoryTests.cs
+++ tests/DaymapInventory.Tests/InMemoryItemInstanceRepositoryTests.cs
@@ -1,42 +1,92 @@
+using System.Linq;
 using DaymapInventory.Interfaces;
 using DaymapInventory.Models;
+using DaymapInventory.Repositories;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace DaymapInventory.Repositories
+namespace DaymapInventory.Tests
 {
-    public class InMemoryItemInstanceRepository : IItemInstanceRepository
+    [TestClass]
+    public class InMemoryItemInstanceRepositoryTests
     {
-        private readonly List<ItemInstance> _itemInstances = new();
-        private int _nextId = 1;
+        private IItemInstanceRepository _repository;
 
-        public void Add(ItemInstance instance)
+        [TestInitialize]
+        public void Setup()
         {
-            instance.Id = _nextId++;
-            _itemInstances.Add(instance);
+            _repository = new InMemoryItemInstanceRepository();
         }
 
-        public IEnumerable<ItemInstance> GetAll()
+        [TestMethod]
+        public void Add_ShouldAssignIdAndStore()
         {
-            return _itemInstances;
-        }
-
-        public ItemInstance? GetById(int id)
-        {
-            return _itemInstances.FirstOrDefault(i => i.Id == id);
-        }
-
-        public IEnumerable<ItemInstance> GetByItemId (int itemId)
-        {
-            return _itemInstances.Where(i => i.ItemId == itemId);
-        }
-        public void Delete(int id)
-        {
-            var item = GetById(id);
-            if (item != null)
+            var instance = new ItemInstance
             {
-                _itemInstances.Remove(item);
-            }
+                ItemId = 1,
+                SerialNumber = "LAPTOP-001",
+                Status = "Available"
+            };
+
+            _repository.Add(instance);
+
+            Assert.IsTrue(instance.Id > 0);
+            Assert.AreEqual(1, _repository.GetAll().Count());
         }
 
-    }
+        [TestMethod]
+        public void GetById_ShouldReturnCorrectInstance()
+        {
+            var instance = new ItemInstance
+            {
+                ItemId = 1,
+                SerialNumber = "LAPTOP-002",
+                Status = "Available"
+            };
 
+            _repository.Add(instance);
+
+            var result = _repository.GetById(instance.Id);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual("LAPTOP-002", result.SerialNumber);
+            Assert.AreEqual(1, result.ItemId);
+        }
+
+        [TestMethod]
+        public void GetById_ShouldReturnNullWhenNotFound()
+        {
+            var result = _repository.GetById(999);
+
+            Assert.IsNull(result);
+        }
+
+        [TestMethod]
+        public void Delete_ShouldRemoveInstance()
+        {
+            var instance = new ItemInstance
+            {
+                ItemId = 1,
+                SerialNumber = "DELETE-001"
+            };
+
+            _repository.Add(instance);
+
+            _repository.Delete(instance.Id);
+
+            Assert.AreEqual(0, _repository.GetAll().Count());
+            Assert.IsNull(_repository.GetById(instance.Id));
+        }
+
+        [TestMethod]
+        public void GetByItemId_ShouldReturnCorrectInstances()
+        {
+            _repository.Add(new ItemInstance { ItemId = 1, SerialNumber = "A001" });
+            _repository.Add(new ItemInstance { ItemId = 1, SerialNumber = "A002" });
+            _repository.Add(new ItemInstance { ItemId = 2, SerialNumber = "B001" });
+
+            var results = _repository.GetByItemId(1);
+
+            Assert.AreEqual(2, results.Count());
+        }
+    }
 }
