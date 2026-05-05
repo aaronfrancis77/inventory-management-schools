89ad460184a8475894f12be02e3fe4620baa5f20
sarahma45 <105347702@student.swin.edu.au>
2026-05-05 16:32:59 +1000

    Create InMemoryItemInstanceRepositoryTests.cs

---
+++ tests/DaymapInventory.Tests/InMemoryItemInstanceRepositoryTests.cs
@@ -0,0 +1,42 @@
+using DaymapInventory.Interfaces;
+using DaymapInventory.Models;
+
+namespace DaymapInventory.Repositories
+{
+    public class InMemoryItemInstanceRepository : IItemInstanceRepository
+    {
+        private readonly List<ItemInstance> _itemInstances = new();
+        private int _nextId = 1;
+
+        public void Add(ItemInstance instance)
+        {
+            instance.Id = _nextId++;
+            _itemInstances.Add(instance);
+        }
+
+        public IEnumerable<ItemInstance> GetAll()
+        {
+            return _itemInstances;
+        }
+
+        public ItemInstance? GetById(int id)
+        {
+            return _itemInstances.FirstOrDefault(i => i.Id == id);
+        }
+
+        public IEnumerable<ItemInstance> GetByItemId (int itemId)
+        {
+            return _itemInstances.Where(i => i.ItemId == itemId);
+        }
+        public void Delete(int id)
+        {
+            var item = GetById(id);
+            if (item != null)
+            {
+                _itemInstances.Remove(item);
+            }
+        }
+
+    }
+
+}
